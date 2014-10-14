378c820617de7a622162737cb4fe509f98e6f6bf
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-14 10:44:30 -0700

    Prevented direct access for the plugin file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+###0.2
+
+* Removed the ability to use php in style file
+* Prevented direct access to the plugin file
+
+
 ###0.1
 
 * Initial commit
--- __init__.php
+++ __init__.php
@@ -5,11 +5,13 @@
  * Plugin URI: https://github.com/hyyan/login-style/
  * Description: Wordpress plugin to allow themes to add their own login page style
  * Author: Hyyan Abo Fakher
- * Version: 0.1
+ * Version: 0.2
  * Author URI: https://github.com/hyyan
  * GitHub Plugin URI: hyyan/login-style
  * License: MIT License
  */
+
+if (!defined('ABSPATH')) exit('restricted access');
 
 require_once __DIR__ . '/src/HyyanLoginStyle.php';
 
